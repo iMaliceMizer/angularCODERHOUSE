48aff6abd658269fffe995a2063d52a1178464ab
Esteban <es.arevaloh@duocuc.cl>
2022-03-09 17:08:52 -0300

    aplicando componentes

--- dunkelheitWEB/src/app/app.module.ts
+++ dunkelheitWEB/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavComponentComponent } from './nav-component/nav-component.component';
+import { CarrouselComponentComponent } from './carrousel-component/carrousel-component.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponentComponent,
+    CarrouselComponentComponent,
+    FooterComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ dunkelheitWEB/src/app/carrousel-component/carrousel-component.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CarrouselComponentComponent } from './carrousel-component.component';
+
+describe('CarrouselComponentComponent', () => {
+  let component: CarrouselComponentComponent;
+  let fixture: ComponentFixture<CarrouselComponentComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CarrouselComponentComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CarrouselComponentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
